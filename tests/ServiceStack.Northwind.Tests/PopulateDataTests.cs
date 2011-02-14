4ce436bae50830a6622f691c7c765acc1a960a8d
Demis Bellot <demis.bellot@gmail.com>
2011-02-14 21:42:10 +0000

    added the test project used to populate the Northwind database

---
+++ tests/ServiceStack.Northwind.Tests/PopulateDataTests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using NUnit.Framework;
+using ServiceStack.Common.Utils;
+using ServiceStack.Northwind.Tests.Support;
+using ServiceStack.OrmLite;
+using ServiceStack.OrmLite.Sqlite;
+
+namespace ServiceStack.Northwind.Tests
+{
+	[TestFixture]
+	public class PopulateDataTests
+	{
+		[Test]
+		public void Create_Sqlite_Database()
+		{
+			var dbPath = "~/App_Data/db.sqlite".MapAbsolutePath();
+			var dbFactory = new OrmLiteConnectionFactory(
+				dbPath, SqliteOrmLiteDialectProvider.Instance);
+
+			if (File.Exists(dbPath)) File.Delete(dbPath);
+
+			NorthwindData.LoadData();
+
+			dbFactory.Exec(dbCmd =>
+			{
+				dbCmd.CreateTables(false, NorthwindFactory.ModelTypes.ToArray());
+
+				dbCmd.SaveAll(NorthwindData.Categories);
+				dbCmd.SaveAll(NorthwindData.Customers);
+				dbCmd.SaveAll(NorthwindData.CustomerCustomerDemos);
+				dbCmd.SaveAll(NorthwindData.Employees);
+				dbCmd.SaveAll(NorthwindData.EmployeeTerritories);
+				dbCmd.SaveAll(NorthwindData.OrderDetails);
+				dbCmd.SaveAll(NorthwindData.Orders);
+				dbCmd.SaveAll(NorthwindData.Products);
+				dbCmd.SaveAll(NorthwindData.Products);
+				dbCmd.SaveAll(NorthwindData.Regions);
+				dbCmd.SaveAll(NorthwindData.Shippers);
+				dbCmd.SaveAll(NorthwindData.Suppliers);
+				dbCmd.SaveAll(NorthwindData.Territories);
+			});
+		}
+
+	}
+
+
+}
